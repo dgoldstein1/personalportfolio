0dbccc30345b8b49354b92a1169e6f8de7c34d6c
dgoldstein1 <david.goldstein@deciphernow.com>
2021-06-07 17:44:26 -0500

    remove sample IP

--- src/App.js
+++ src/App.js
@@ -10,36 +10,36 @@ import Portfolio from "./Components/Portfolio";
 import VisitInfo from "./Components/VisitInfo";
 import axios from "axios";
 
-const sampleIP = {
-  ip: "67.4.164.47",
-  type: "ipv4",
-  continent_code: "NA",
-  continent_name: "North America",
-  country_code: "US",
-  country_name: "United States",
-  region_code: "MN",
-  region_name: "Minnesota",
-  city: "Minneapolis",
-  zip: "55436",
-  latitude: 44.89057159423828,
-  longitude: -93.35533142089844,
-  location: {
-    geoname_id: 5037649,
-    capital: "Washington D.C.",
-    languages: [
-      {
-        code: "en",
-        name: "English",
-        native: "English",
-      },
-    ],
-    country_flag: "http://assets.ipstack.com/flags/us.svg",
-    country_flag_emoji: "🇺🇸",
-    country_flag_emoji_unicode: "U+1F1FA U+1F1F8",
-    calling_code: "1",
-    is_eu: false,
-  },
-};
+// const sampleIP = {
+//   ip: "67.4.164.47",
+//   type: "ipv4",
+//   continent_code: "NA",
+//   continent_name: "North America",
+//   country_code: "US",
+//   country_name: "United States",
+//   region_code: "MN",
+//   region_name: "Minnesota",
+//   city: "Minneapolis",
+//   zip: "55436",
+//   latitude: 44.89057159423828,
+//   longitude: -93.35533142089844,
+//   location: {
+//     geoname_id: 5037649,
+//     capital: "Washington D.C.",
+//     languages: [
+//       {
+//         code: "en",
+//         name: "English",
+//         native: "English",
+//       },
+//     ],
+//     country_flag: "http://assets.ipstack.com/flags/us.svg",
+//     country_flag_emoji: "🇺🇸",
+//     country_flag_emoji_unicode: "U+1F1FA U+1F1F8",
+//     calling_code: "1",
+//     is_eu: false,
+//   },
+// };
 
 class App extends Component {
   constructor(props) {
@@ -55,7 +55,6 @@ class App extends Component {
     return axios
       .get(resumeData.config.geoIpServer)
       .then((r) => {
-        r = sampleIP;
         return axios
           .post(resumeData.config.metricsServer + "/visits", r)
           .then((mr) => {
